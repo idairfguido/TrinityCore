162dbeeafe191488e74f0e42f0c85a77fe381af4
Idair F. Guido <idair_guido@hotmail.com>
2025-10-16 15:05:21 -0300

    * Updated some text files.
      - Rename THANKS to AUTHORS
    
      - Update copyright notes.

--- src/server/scripts/EasternKingdoms/Karazhan/boss_moroes.cpp
+++ src/server/scripts/EasternKingdoms/Karazhan/boss_moroes.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2018 TrinityCore <https://www.trinitycore.org/>
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
  * Copyright (C) 2006-2008 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify it
